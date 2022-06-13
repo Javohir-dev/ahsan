76ddbcf5fc49b79da633baffbcddf061b21983f4
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-14 00:04:29 +0500

    .

--- MDBootstrap/js/main.js
+++ MDBootstrap/js/main.js
@@ -37,8 +37,8 @@ $(function () {
 // ! gallary almashish js
 const current = document.querySelector("#current");
 const imgs = document.querySelectorAll(".imgs img");
-// opacity = 0.6;
-// imgs[0].style.opacity = opacity;
+opacity = 0.6;
+imgs[0].style.opacity = opacity;
 imgs.forEach((img) => img.addEventListener("click", imgClick));
 function imgClick(e) {
   imgs.forEach((img) => (img.style.opacity = 1));
