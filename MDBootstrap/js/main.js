aed873e5974a45725342700419b45e186c90fdda
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-13 20:18:04 +0500

    Added new page

--- MDBootstrap/js/main.js
+++ MDBootstrap/js/main.js
@@ -2,29 +2,14 @@ $(document).ready(function () {
   new WOW().init();
 });
 $(function () {
-  $("#add_cart").click(function () {
-    $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
-  });
-});
-$(function () {
-  $("#add_cart2").click(function () {
-    $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
-  });
-});
-$(function () {
-  $("#add_cart3").click(function () {
-    $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
-  });
-});
-$(function () {
-  $("#add_cart4").click(function () {
+  $(".add_cart").click(function () {
     $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
   });
 });
 
 // ! search uchun qidiruv tizimi.
 $(function () {
-  var availableTags = ["Parrot", "Piagon", "Dove", "Swan", "macbook air"];
+  var availableTags = ["macbook pro", "macbook air"];
   var NoResultsLabel = "No Results";
 
   $("#tags").autocomplete({
@@ -49,39 +34,16 @@ $(function () {
     },
   });
 });
-
-$(function () {
-  var availableTags = [
-    "Rolex",
-    "Tasset",
-    "Mechanic",
-    "Seven Friday",
-    "Iphone",
-    "Sumsung",
-    "Xiaomi",
-    "Huawei",
-  ];
-  var NoResultsLabel = "No Results";
-
-  $("#tags").autocomplete({
-    source: function (request, response) {
-      var results = $.ui.autocomplete.filter(availableTags, request.term);
-
-      if (!results.length) {
-        results = [NoResultsLabel];
-      }
-
-      response(results);
-    },
-    select: function (event, ui) {
-      if (ui.item.label === NoResultsLabel) {
-        event.preventDefault();
-      }
-    },
-    focus: function (event, ui) {
-      if (ui.item.label === NoResultsLabel) {
-        event.preventDefault();
-      }
-    },
-  });
-});
+// ! gallary almashish js
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+// opacity = 0.6;
+// imgs[0].style.opacity = opacity;
+imgs.forEach((img) => img.addEventListener("click", imgClick));
+function imgClick(e) {
+  imgs.forEach((img) => (img.style.opacity = 1));
+  current.src = e.target.src;
+  current.classList.add("fade-in");
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+  e.target.style.opacity = opacity;
+};
