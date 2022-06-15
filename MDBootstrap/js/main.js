a047684148a102553ce12cba6d04c4020befa11f
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-15 18:54:42 +0500

    version 1

--- MDBootstrap/js/main.js
+++ MDBootstrap/js/main.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
 });
 $(function () {
   $(".add_cart").click(function () {
-    $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
+    $(".successfully-saved").css("display", "block").delay(500).fadeOut(100);
   });
 });
-
+// !
+$(document).ready(function () {
+  new WOW().init();
+});
+$(function () {
+  $(".card_removed").click(function () {
+    $(".successfully-removed").css("display", "block").delay(500).fadeOut(100);
+  });
+});
 // ! search uchun qidiruv tizimi.
 $(function () {
   var availableTags = ["macbook pro", "macbook air"];
@@ -34,16 +42,21 @@ $(function () {
     },
   });
 });
-// ! gallary almashish js
-const current = document.querySelector("#current");
-const imgs = document.querySelectorAll(".imgs img");
-opacity = 0.6;
-imgs[0].style.opacity = opacity;
-imgs.forEach((img) => img.addEventListener("click", imgClick));
-function imgClick(e) {
-  imgs.forEach((img) => (img.style.opacity = 1));
-  current.src = e.target.src;
-  current.classList.add("fade-in");
-  setTimeout(() => current.classList.remove("fade-in"), 500);
-  e.target.style.opacity = opacity;
-};
+
+// -1+ uchun
+$(document).ready(function () {
+  $(".minus").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".plus").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
