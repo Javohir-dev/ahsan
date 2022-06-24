1c3c23c96f8b0fcf735e074f7da5fd4c91383e92
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-24 20:06:22 +0500

    Added new animate

--- MDBootstrap/js/main.js
+++ MDBootstrap/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   new WOW().init();
 });
 $(function () {
-  $(".card_removed").click(function () {
+  $(".cort_remove").click(function () {
     $(".successfully-removed").css("display", "block").delay(800).fadeOut(100);
   });
 });
@@ -43,7 +43,7 @@ $(function () {
   });
 });
 
-// -1+ uchun
+// ? -1+ uchun
 $(document).ready(function () {
   $(".minus").click(function () {
     var $input = $(this).parent().find("input");
@@ -61,11 +61,108 @@ $(document).ready(function () {
   });
 });
 // ! btn sotib olish
-function handleDropDown(elementId) {
-  const element = document.getElementById("languages");
-  if (element.style.display === "block") {
-    element.style.display = "none";
-  } else {
-    element.style.display = "block";
-  }
-}
+// function handleDropDown(elementId) {
+//   const element = document.getElementById("languages");
+//   if (element.style.display === "block") {
+//     element.style.display = "none";
+//   } else {
+//     element.style.display = "block";
+//   }
+// };
+// ! scrul
+$(function () {
+  var availableTags = ["Huawei", "Smart watch", "Apple airpods", "Notebook"];
+  var NoResultsLabel = "No Results";
+  $("#tags").autocomplete({
+    source: function (request, response) {
+      var results = $.ui.autocomplete.filter(availableTags, request.term);
+      if (!results.length) {
+        results = [NoResultsLabel];
+      }
+      response(results);
+    },
+    select: function (event, ui) {
+      if (ui.item.label === NoResultsLabel) {
+        event.preventDefault();
+      }
+    },
+    focus: function (event, ui) {
+      if (ui.item.label === NoResultsLabel) {
+        event.preventDefault();
+      }
+    },
+  });
+});
+
+$(function () {
+  $(".cart_add").click(function () {
+    $(".successfully-saved").css("display", "block").delay(3000).fadeOut(500);
+  });
+});
+
+$(function () {
+  $(".cart_rem").click(function () {
+    $(".successfully-removed").css("display", "block").delay(3000).fadeOut(500);
+  });
+});
+
+$(document).ready(function () {
+  new WOW().init();
+});
+
+$(document).ready(function () {
+  $(".minus").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".plus").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: false,// ! ?
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$(document).ready(function () {
+  // OWL CAROUSEL - https://owlcarousel2.github.io/OwlCarousel2/
+
+  $("#owl-1 .owl-carousel").owlCarousel({
+    items: 4,
+    margin: 16,
+    loop: true,
+    stagePadding: 64,
+    nav: true,
+    // navText: ['Back','Next'],
+    navText: ["", ""],
+    // navText: ["<img src='myprevimage.png'>","<img src='mynextimage.png'>"],
+    dots: false,
+    dotsEach: true,
+    lazyLoad: false,
+    autoplay: true,
+    autoplaySpeed: 500,
+    navSpeed: 500,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
+});
